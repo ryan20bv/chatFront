0977b595d4c25c4aa6c155b242a5dbc27b3dbfb3
unknown <ryan20bv@gmail.com>
2023-04-13 14:12:21 +0800

    able to use tailwind

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -10,27 +10,39 @@ const LoginForm = () => {
 		router.replace("/chat");
 	};
 	return (
-		<section className={classes.loginForm}>
-			<form onSubmit={submitFormHandler}>
+		// <section className={classes.loginForm}>
+		<section
+			className={`border border-black p-4 sm:w-96 w-11/12 rounded-lg  ${classes.loginForm}`}
+		>
+			<form
+				onSubmit={submitFormHandler}
+				className='w-full '
+			>
 				<input
 					type='email'
 					id='email'
 					placeholder='Email address'
+					className='block w-full px-3 py-2 mb-4 rounded-md text-xl
+					'
 				/>
 				<input
 					type='text'
 					id='password'
 					placeholder='Password'
+					className='block w-full px-3 py-2 mb-4 rounded-md text-xl'
 				/>
-				<button>Log in</button>
+				<button className='block w-full py-2 mb-4 bg-blue-500 text-white rounded-md text-lg'>
+					Log in
+				</button>
 			</form>
-			<div>
-				<p>Forgotten password?</p>
-			</div>
-			<hr />
-			<div>
-				<button>Create new account</button>
-			</div>
+
+			<p className='text-center text-blue-700'>Forgotten password?</p>
+
+			<hr className='border-black my-5' />
+
+			<button className='bg-green-600 py-2 block w-full text-white rounded-md text-lg'>
+				Create new account
+			</button>
 		</section>
 	);
 };
