b291b9f6a71efce713892981619c60ac79f3135c
unknown <ryan20bv@gmail.com>
2023-04-14 14:56:18 +0800

    create chat list and chat item

---
+++ src/components/chat/ChatItem.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Image from "next/image";
+const randomNumber = () => {
+	return Math.ceil(Math.random() * 3).toString();
+};
+const DummyPerson = {
+	_id: "p1",
+	avatarUrl: "avatar" + randomNumber() + ".jpg",
+	name: "Mallory Diaz",
+	email: "mallorydiaz1337@test.com",
+};
+
+const ChatItem = () => {
+	return (
+		<li className='my-3'>
+			<div className=' border  border-red-600 w-3/4 flex'>
+				<Image
+					src={`/images/avatar/${DummyPerson.avatarUrl}`}
+					alt='image'
+					width={100}
+					height={100}
+					className='rounded-full w-12 h-12'
+				/>
+				<p className='ml-3'>
+					hi asd ad ad asd asdada sd asdasd ad ad asd as ad as ad asd asd asd a sas
+					ad s ad asd as da as asdasd asd asds asdasd sad asd asd
+				</p>
+			</div>
+		</li>
+	);
+};
+
+export default ChatItem;
